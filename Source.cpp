be54d266cbfbadb1cf9179b4e0ecd9101644fe0d
Ahmed Aboalasaad <53487714+Ahmed-Aboalasaad@users.noreply.github.com>
2022-04-11 17:50:55 +0200

    Update Source.cpp

--- Source.cpp
+++ Source.cpp
@@ -9,37 +9,39 @@ using namespace std;
 
 
 // FUNCTIONS prototypes
-
+void editInfo();
 
 // STRUCTS
 struct dateStruct
 {
-	int day, month, year;
+	int day = 0, month = 0, year = 2020;
 };
 
 struct timeStruct
 {
 	dateStruct date;
-	int hour, minute;
+	int hour = 0, minute = 0;
 };
-
-struct patient
-{
-	string name, password;
-	dateStruct DOB;
-} patients[50];
-
-struct doctor
-{
-	string name, password;
-	timeStruct appointments[20];
-} drAli;
 
 struct appointment
 {
 	string patientName, doctorName;
 	timeStruct time;
 } appointments[50];
+
+struct patient
+{
+	string name, password;
+	dateStruct DOB;
+	appointment appointments[20];
+} patients[50];
+
+struct doctor
+{
+	string name, password;
+	appointment appointments[20];
+} drAli;
+
 
 int main() {
 
@@ -49,47 +51,70 @@ int main() {
 	// GEHAD : (SIGN UP OR LOG IN)
 	cout << " as a doctor or patient?";
 	cin >> ans;
-	if (ans == 'd' || 'D')
-	{
+	if (ans == 'd' || ans == 'D') {
 		cout << "1. Edit available time" << endl              // MALAK
-			 << "2. Display the your appointments" << endl     // MAYAR
-			 << "3. Edit you info" << endl;                    // DINA
+			<< "2. Display your appointments" << endl     // MAYAR
+			<< "3. Edit you info" << endl;                    // DINA
 
 		char service;
 		cin >> service;
-		if (service == '1') {
-			timeStruct change;
+		string order;
 
-			// Obtain the old appointment
-			cout << "Enter the time you want to change date(day month year), hour, minute";
-			cin >> change.date.day >> change.date.month >> change.date.year >> change.hour >> change.minute;
+		do {
+			// Edit available time
+			if (service == '1') {
+				timeStruct change;
 
-			// loop over this doctor's appointments to find the old one
-			for (int i = 0; i < 20;) {
-				// if you found it
-				if (
-						(change.date.day == drAli.appointments[i].date.day) &&
-						(change.date.month == drAli.appointments[i].date.month) &&
-						(change.date.year == drAli.appointments[i].date.year) &&
-						(change.hour == drAli.appointments[i].hour) &&
-						(change.minute == drAli.appointments[i].minute)
-					) {
-					// take the updated appointment
-					cout << "Please write the new time date(day month year), hour, minute\n";
-					cin >> change.date.day >> change.date.month >> change.date.year >> change.hour >> change.minute;
+				// Obtain the old appointment
+				cout << "Enter the old appointment you want to change: date(day month year), hour, minute\n";
+				cin >> change.date.day >> change.date.month >> change.date.year >> change.hour >> change.minute;
+
+				// loop over this doctor's appointments to find the old one
+				for (int i = 0; i < 20;) {
+					// if you found it
+					if (
+						(change.date.day == drAli.appointments[i].time.date.day) &&
+						(change.date.month == drAli.appointments[i].time.date.month) &&
+						(change.date.year == drAli.appointments[i].time.date.year) &&
+						(change.hour == drAli.appointments[i].time.hour) &&
+						(change.minute == drAli.appointments[i].time.minute)
+						) {
+						// take the updated appointment
+						cout << "Please write the new time date(day month year), hour, minute\n";
+						cin >> change.date.day >> change.date.month >> change.date.year >> change.hour >> change.minute;
+					}
 				}
 			}
-		}
-		else if (service == '2') {
-			// Loop over all the appointments
-			for (int i = 0; i < 50; i++) {
-				// if the doctor name of one of them was the same as the logged in doctor
-				if (appointments[i].doctorName == drAli.name) {
-					cout << "patient name : " << patients[i].name << "\t"
-						 << "appointment day : " << patients[i].appointment.date << "\t" << "the patient time:" << arrP[i].reservation.hours << ":" << arrP[i].reservation.minutes << endl;
+
+			// Display your appointments
+			else if (service == '2') {
+				cout << "------------ appointments of dr." << drAli.name << " ------------\n";
+				for (int i = 0; i < 20; i++) {
+					cout << "patient name : " << drAli.appointments[i].patientName
+						<< "\ntime : "
+						<< drAli.appointments[i].time.date.day
+						<< drAli.appointments[i].time.date.month
+						<< drAli.appointments[i].time.date.year
+						<< drAli.appointments[i].time.hour
+						<< drAli.appointments[i].time.minute << endl;
 				}
 			}
-		}
-		
+
+			// Edit you info
+			else if (service == '3') {
+				editInfo();  // DINA
+			}
+
+			// Another Operation ?
+			cout << "have a nice day dr." << drAli.name << endl
+				<< "1. Go back to the main menu? ( h / H / home )";
+			cin >> order;
+
+			// GEHAD, make it case-insensetive checking, please
+		} while (order == "h" || order == "H" || order == "home" || order == "Home");
+
+	}
+
+
 	return 0;
 }
