63be92e3d26d091c76ad839639d9a72b70e61a5f
Mayar128 <104272977+Mayar128@users.noreply.github.com>
2022-04-28 22:44:04 +0200

    Update Source.cpp

--- Source.cpp
+++ Source.cpp
@@ -1,3 +1,4 @@
+
 #include <iostream>
 #include <string>
 using namespace std;
@@ -6,29 +7,6 @@ using namespace std;
 // Please:
 //	leave a LOT of comments (we will submit a less-commenty version later)
 //	name things in the camel-case way with very discriptive names even if they are long
-
-
-// FUNCTIONS prototypes
-// DINA
-void editInfo();
-
-// ABO
-void findDoctor(string patientName);
-void timeFilter();
-void majorFilter(string patientName);
-
-// Safa
-void viewAppointments();
-void editPatientInfo();
-
-// Seif
-void addAppointment();
-void editAppointment();
-void deleteAppointments();
-void clearAppointments();
-
-// MALAK
-void addAbailableTime(string doctorName);
 
 
 
@@ -42,9 +20,10 @@ struct dateStruct
 
 struct timeStruct
 {
+	bool Available = true;
 	dateStruct date;
 	int hour = 0, minute = 0;
-};
+}change;
 
 struct appointment
 {
@@ -56,32 +35,88 @@ struct patient
 {
 	string name, password;
 	dateStruct DOB;
-	appointment appointments[20];
+	appointment Pappointments[20];
 };
 
 struct doctor
 {
 	string name, password, major;
+	appointment DRappointments[20];
+	timeStruct availableTime[20];
+};
 
-	// 168 half hour a week (0 -> not available || 1 -> avaialble || 2 -> reserved)
-	short int weekBlocks[168] = { 0 };  // all zeroes by default
-	appointment appointments[20];
-	timeStruct availableTime[14];  // start & end of his/her abailable time for a week
-} drAli;
+
+
+// FUNCTIONS prototypes
+
+void doctorMenu(doctor darr[], int j);
+void patientMenu();
+
+// DINA
+void editInfo(doctor darr[], int j);
+
+// ABO
+void findDoctor();
+void timeFilter();
+void majorFilter();
+
+// Safa
+void viewAppointments();
+void editPatientInfo();
+
+// Seif
+void addAppointment();
+void editAppointment();
+void deleteAppointments();
+void clearAppointments();
+
+
+//MAYAR
+void editAvailableTime(doctor darr[], int j);
+void displayDoctorAppointment(doctor darr[], int j);
+
+
+//MALK
+void AddAvailableTime(doctor darr[], int j);
+void RemoveAvailabeTime(doctor darr[], int j);
+
+
 
 
 int main() {
-
+	doctor Darr[50];
+	patient Parr[50];
 	char ans;
 	cout << "------------ Welcome om E7gezly ------------\n";
 
 	// GEHAD : (SIGN UP OR LOG IN)
+
+	int i = 10;               	//just a sample to work with , this part will be done in GEHAD'S part
+	Darr[i].name = "Malk";     	//just a sample to work with , this part will be done in GEHAD'S part
+	         
 	cout << " as a doctor or patient?";
 	cin >> ans;
-	if (ans == 'd' || ans == 'D') {
-		cout << "1. Edit available time" << endl              // MALAK
-			<< "2. Display your appointments" << endl     // MAYAR
-			<< "3. Edit you info" << endl;                    // DINA
+	if (ans == 'd' || ans == 'D') 
+	{
+		 doctorMenu(Darr,i);
+
+	}
+	else if (ans == 'p' || ans == 'P')
+	{
+		 patientMenu();
+	}
+
+
+	return 0;
+}
+void doctorMenu(doctor darr[],int j)
+{
+
+		cout << "1. Edit available time" << endl              // MAYAR
+			<< "2. Add available time" << endl                   //MALK
+			<< "3. Remove available time" << endl                //MALK
+			<< "4. Display your appointments" << endl     // MAYAR
+			<< "5. Edit you info" << endl;                    // DINA
 
 		char service;
 		cin >> service;
@@ -89,85 +124,161 @@ int main() {
 
 		do {
 			// Edit available time
-			if (service == '1') {
-				timeStruct change;
+			if (service == '1')
+			{
 
-				// Obtain the old appointment
-				cout << "Enter the old appointment you want to change: date(day month year), hour, minute\n";
-				cin >> change.date.day >> change.date.month >> change.date.year >> change.hour >> change.minute;
-
-				// loop over this doctor's appointments to find the old one
-				for (int i = 0; i < 20;) {
-					// if you found it
-					if (
-						(change.date.day == drAli.appointments[i].time.date.day) &&
-						(change.date.month == drAli.appointments[i].time.date.month) &&
-						(change.date.year == drAli.appointments[i].time.date.year) &&
-						(change.hour == drAli.appointments[i].time.hour) &&
-						(change.minute == drAli.appointments[i].time.minute)
-						) {
-						// take the updated appointment
-						cout << "Please write the new time date(day month year), hour, minute\n";
-						cin >> change.date.day >> change.date.month >> change.date.year >> change.hour >> change.minute;
-					}
-				}
+				 editAvailableTime(darr,j);
+			}
+			//Add available time
+			else if (service == '2')
+			{
+				 AddAvailableTime(darr, j);  //MAlk
+			}
+			//Remove availble time
+			else if (service == '3')
+			{
+				 RemoveAvailabeTime(darr, j);  //MAlk
 			}
 
 			// Display your appointments
-			else if (service == '2') {
-				cout << "------------ appointments of dr." << drAli.name << " ------------\n";
-				for (int i = 0; i < 20; i++) {
-					cout << "patient name : " << drAli.appointments[i].patientName
-						<< "\ntime : "
-						<< drAli.appointments[i].time.date.day
-						<< drAli.appointments[i].time.date.month
-						<< drAli.appointments[i].time.date.year
-						<< drAli.appointments[i].time.hour
-						<< drAli.appointments[i].time.minute << endl;
-				}
+			else if (service == '4')
+			{
+				 displayDoctorAppointment(darr,j);
 			}
 
 			// Edit you info
-			else if (service == '3') {
-				editInfo();  // DINA
+			else if (service == '5')
+			{
+				editInfo(darr, j);  // DINA
+			}
+			else
+			{
+				cout << "Invalid choice please try again!\n";
+				continue;
 			}
 
+
 			// Another Operation ?
-			cout << "have a nice day dr." << drAli.name << endl
+			cout << "have a nice day dr." << darr[j].name << endl
 				<< "1. Go back to the main menu? ( h / H / home )";
 			cin >> order;
 
 			// GEHAD, make it case-insensetive checking ,please
 		} while (order == "h" || order == "H" || order == "home" || order == "Home");
 
-	}
-	else if (ans == 'p' || ans == 'P') {
-
-	}
+		cout << "Thank you for using our system, have a nice day doctor " << darr[j].name << ":)\n";
 
 
-	return 0;
 }
 
-void findDoctor(string patientName) {
-	cout << "Search by :\n1.Major\n2. Major & Available Time\n";
-	char response;
-	cin >> response;
-	if (response == '1') {
-		majorFilter(patientName);
+
+void editAvailableTime(doctor darr[], int j)
+{
+
+	// Obtain the old appointment
+	cout << "Please enter the old appointment you want to change: date(day month year), hour, minute\n";
+	cin >> change.date.day >> change.date.month >> change.date.year >> change.hour >> change.minute;
+
+	// loop over this doctor's appointments to find the old one
+	for (int i = 0; i < 20; i++) 
+	{
+		// if you found it
+		if (
+			(change.date.day == darr[j].DRappointments[i].time.date.day) &&
+			(change.date.month == darr[j].DRappointments[i].time.date.month) &&
+			(change.date.year == darr[j].DRappointments[i].time.date.year) &&
+			(change.hour == darr[j].DRappointments[i].time.hour) &&
+			(change.minute == darr[j].DRappointments[i].time.minute)
+			)
+		{
+			//if that time wasn't booked by any patient
+			if (darr[j].DRappointments[i].time.Available ==true)
+			{
+				// take the updated appointment
+				cout << "Please write the new time date(day month year), hour, minute\n";
+				cin >> darr[j].DRappointments[i].time.date.day >> darr[j].DRappointments[i].time.date.month >> darr[j].DRappointments[i].time.date.year >> darr[j].DRappointments[i].time.hour >> darr[j].DRappointments[i].time.minute;
+			}
+			else
+				cout << "This time had been booked by a patient please choose another time to change";
+		}
 	}
 }
 
-void addAbailableTime(doctor doctor) {
-	// which day to set its available time ?
-	cout << "Abailable Time: \nDay: ";
-	cin >> doctor.availableTime[0].date.day;
-	// the start & end of your available time are in the same day
-	doctor.availableTime[1].date.day = doctor.availableTime[0].date.day;
 
-	// in that day your are available from .. to ..
-	cout << "From: (use blocks of hours or half hours)";
-	cin >> doctor.availableTime[0].hour;
-	cout << "To: (use blocks of hours or half hours)";
-	cin >> doctor.availableTime[1].hour;
+void addAvailableTime(doctor darr[], int j)
+{
+	char another = 'y';
+	int sure;
+	cout << "\t\t\t\t------------------------\n\t\t\twelcome\n to *add* a new available time just follow the following instructions\n";
+	do
+	{
+		for (int i = 0; i < 20; i++)
+		{
+
+			cout << "write the information of the available time you want to add:\n " <<
+				"write \t\t for\n 1\t\tSaturday\n 2\t\tSunday\n 3\t\tMonday\n 4\t\t Tuesday\n 5\t\t Wednesday\n 6\t\tThursday\n 7\t\t Friday\n";
+			cout << "Choose the day\n";
+			cin >> darr[j].availableTime[i].date.day;
+			cout << "enter the month (1 to 12)";
+			cin >> darr[j].availableTime[i].date.month;
+			cout << "enter the hour (0 to 23)";
+			cin >> darr[j].availableTime[i].hour;
+			cout << "enter the minute (0 to 59)";
+			cin >> darr[j].availableTime[i].minute;
+
+		}
+		cout << "The operation done successfully :D\n";
+
+
+		cout << "\nDo you want to do another operation? (y/Y for yes && any key to stop)\n";
+		cin >> another;
+	} while (another == 'y' || another == 'Y');
+}
+
+
+void RemoveAvailabeTime(doctor darr[], int j)
+{
+	char another = 'y';
+	int sure;
+	cout << "\t\t\t\t------------------------\n\t\t\twelcome\n to *delete* a new available time just follow the following instructions\n";
+	do
+	{
+		for (int i = 0; i < 20; i++)
+		{
+			cout << "write the information of the available time you want to delete:\n " <<
+				"write \t\t for\n 1\t\tSaturday\n 2\t\tSunday\n 3\t\tMonday\n 4\t\t Tuesday\n 5\t\t Wednesday\n 6\t\tThursday\n 7\t\t Friday\n";
+			cout << "Choose the day\n";
+			cin >> darr[j].availableTime[i].date.day;
+			cout << "enter the month (1 to 12)";
+			cin >> darr[j].availableTime[i].date.month;
+			cout << "enter the hour (0 to 23)";
+			cin >> darr[j].availableTime[i].hour;
+			cout << "enter the minute (0 to 59)";
+			cin >> darr[j].availableTime[i].minute;
+		}
+		cout << "The operation done successfully :D\n";
+
+		cout << "\nDo you want to do another operation? (y/Y for yes && any key to stop)\n";
+		cin >> another;
+	} while (another == 'y' || another == 'Y');
+}
+
+
+void displayDoctorAppointment(doctor darr[], int j)
+{
+
+	cout << "------------ appointments of dr." << darr[j].name << " ------------\n";
+	for (int i = 0; i < 20; i++)
+	{
+		cout << "patient name : " << darr[j].DRappointments[i].patientName
+			<< "\n Appointment date: "
+			<< darr[j].DRappointments[i].time.date.day << "/"
+			<< darr[j].DRappointments[i].time.date.month << "/"
+			<< darr[j].DRappointments[i].time.date.year << endl
+			<< "Appointment time:"
+			<< darr[j].DRappointments[i].time.hour << ":"
+			<< darr[j].DRappointments[i].time.minute << endl;
+	}
+
+
 }
